261a905641f0c05ead90f58aec5a038ef73bb41d
olglk <olgalukianchukk@gmail.com>
2020-07-02 15:18:17 +0300

    updated Audit Model entity

--- src/main/java/com/example/demo/model/AuditModel.java
+++ src/main/java/com/example/demo/model/AuditModel.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -18,13 +19,12 @@ import java.util.Date;
         allowGetters = true
 )
 public abstract class AuditModel implements Serializable {
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "created_at", nullable = false, updatable = false)
-    @CreatedDate
-    private Date createdAt;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "updated_at", nullable = false)
+    @Column(columnDefinition = "DATE", name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Column(columnDefinition = "DATE", name = "updated_at", nullable = false)
     @LastModifiedDate
-    private Date updatedAt;
+    private LocalDate updatedAt;
 }
